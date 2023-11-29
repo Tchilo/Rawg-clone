20d9b33b0f84025f917010c60b61e5aebda32b60
Tchilo <tchiloross@gmail.com>
2023-11-29 22:30:38 +0200

    Dispaly critic score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react"
+import { Card, CardBody, Heading, Image,HStack } from "@chakra-ui/react"
 import { Game } from "../hooks/useGames"
 import PlatformIconList from "./PlatformIconList"
+import CriticScore from "./CriticScore"
 
 interface Props {
     game: Game
@@ -8,10 +9,13 @@ interface Props {
 const GameCard = ({ game }: Props) => {
     return (
         <Card borderRadius={8} overflow={'hidden'}>
-            <Image src={game.background_image}/>
+            <Image src={game.background_image} />
             <CardBody>
                 <Heading fontSize='2xl'>{game.name}</Heading>
-                <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)}/>
+                <HStack justifyContent='space-between'>
+                    <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)} />
+                    <CriticScore score={game.metacritic} />
+                </HStack>
             </CardBody>
         </Card>
     )
