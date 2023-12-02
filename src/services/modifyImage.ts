01e5abac540fc303cb608fb46a7ce35926650039
Tchilo <tchiloross@gmail.com>
2023-12-02 11:50:37 +0200

    Handle games with no image

--- src/services/modifyImage.ts
+++ src/services/modifyImage.ts
@@ -1,6 +1,6 @@
-
+import brokenImage from '../assets/brokenImage.jpg'
 const modifyImage = (url: string) => {
-    if(!url) return ''
+    if(!url) return brokenImage
     const target = 'media/'
     const index = url.indexOf('media/') + target.length
     return url.slice(0,index) +'crop/600/400/'+ url.slice(index)
