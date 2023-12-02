3a45964d2ff764a6dba62025600759772664d665
Tchilo <tchiloross@gmail.com>
2023-12-02 13:34:06 +0200

    Ship geners and platforms with the app

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,11 +1,10 @@
-import useData from "./useData";
-
+import Platforms from "../data/Platforms"
 export interface Platform {
     id: number
     name: string
     slug: string
 }
 
-const usePlatforms = () => useData<Platform>('/platforms/lists/parents')
+const usePlatforms = () => ({data:Platforms, isLoading:false,error:null})
 
 export default usePlatforms
